f6a87073a00a0ca668f37778b2012e7d1b992432
Vinay Hegde <76780478+vinayhegde105@users.noreply.github.com>
2021-10-09 19:00:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-provider "azurerm" {
-  features {}
-}
-
-terraform {
-  required_providers {
-      azurerm = {
-          source = "hashicorp/azurerm"
-          version = "2.46.0"
-      }
-  }
-}
 resource "azurerm_resource_group" "vth" {
   name     = "vth-resources"
   location = "West Europe"
